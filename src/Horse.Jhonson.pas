6a9f897874c87b5d0127db478090fae6de9ad63f
Matheus Hunsche <matheusarendthunsche@gmail.com>
2018-10-02 17:10:42 -0300

    Initial Commit

---
+++ src/Horse.Jhonson.pas
@@ -0,0 +1,40 @@
+unit Horse.Jhonson;
+
+interface
+
+uses
+  Horse, System.Classes, System.JSON, Web.HTTPApp, System.SysUtils;
+
+procedure Jhonson(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+
+implementation
+
+procedure Jhonson(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  LWebRequest: TWebRequest;
+  LWebResponse: TWebResponse;
+  LContent: TObject;
+  LJSON: TJSONValue;
+begin
+  LWebRequest := THorseHackRequest(Req).GetWebRequest;
+
+  if (LWebRequest.MethodType in [mtPost, mtPut]) and
+    (LWebRequest.ContentType = 'application/json') then
+  begin
+    LJSON := TJSONObject.ParseJSONValue(Req.Body);
+    THorseHackRequest(Req).SetBody(LJSON);
+  end;
+
+  Next;
+
+  LWebResponse := THorseHackResponse(Res).GetWebResponse;
+  LContent := THorseHackResponse(Res).GetContent;
+
+  if LContent.InheritsFrom(TJSONValue) then
+  begin
+    LWebResponse.Content := TJSONValue(LContent).ToJSON;
+    LWebResponse.ContentType := 'application/json';
+  end;
+end;
+
+end.
