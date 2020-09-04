6acdb85a94fd17317672e2970413dfedcfaa854b
viniciussanchez <viniciuss.sanchez@gmail.com>
2020-09-03 22:50:20 -0300

    Horse 2.0 #7

--- samples/Samples.dpr
+++ samples/Samples.dpr
@@ -4,18 +4,12 @@ program Samples;
 
 {$R *.res}
 
-uses
-  Horse, Horse.Jhonson, System.JSON;
-
-var
-  App: THorse;
+uses Horse, Horse.Jhonson, System.JSON;
 
 begin
-  App := THorse.Create(9000);
+  THorse.Use(Jhonson);
 
-  App.Use(Jhonson);
-
-  App.Post('ping',
+  THorse.Post('/ping',
     procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
     var
       LBody: TJSONObject;
@@ -24,5 +18,5 @@ begin
       Res.Send<TJSONObject>(LBody);
     end);
 
-  App.Start;
+  THorse.Listen(9000);
 end.
